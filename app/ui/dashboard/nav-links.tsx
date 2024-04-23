8a53537d4a27e6b000c5a2d2cae6e0037fe05e08
techerudite-dhaval-l <Dhaval@techerudite.com>
2024-04-23 10:25:33 +0530

    dashboard working

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import {
   UserGroupIcon,
@@ -22,7 +22,6 @@ const links = [
 ];
 
 export default function NavLinks() {
-
   const pathname = usePathname();
 
   return (
